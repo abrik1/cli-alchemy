308d26d42b76839e0a7171eebd8840da0003f267
abrik1 <133798548+abrik1@users.noreply.github.com>
2024-03-14 15:21:30 +0530

    add some elements

---
+++ elements.py
@@ -0,0 +1,40 @@
+elements = {
+    "1UP": ["", "🍄 mushroom"],
+    "🌬️ air": [], # [] means its a basic element
+    "✈️ airplane": [
+        ["🌬️ air", "🚗 car"],
+        ["🐦 bird", "🔨 metal"]
+    ],
+    "🤓 Albert Einstein": {
+      ["⚡ energy", "🧑‍🔬 scientist"]  
+    },
+    "🍷 alchohol": [
+        ["🍷 alchohol", "🦠 bacteria"],
+        ["🔥 fire", "🌬️ air"]
+    ],
+    "🍻 alcholic": [
+        ["🍷 alchohol", "🧑 man"],
+        ["🍺 beer", "🧑 man"],
+        ["🧑 man", "🍸 vodka"],
+        ["🧑 man", "🍹 tequila"]
+    ],
+    "🌱 algae": [
+        ["🧬 life", "💧 water"]
+    ],
+    "👽 alien" : [
+        ["👹 beast", "⭐ star"],
+        ["🧬 life", "⭐ star"]
+    ],
+    
+
+    "🇩🇪 Germany": [
+        ["🌏 country", "🚗 VW Beetle"]
+    ],
+    "🇷🇴 Romania": [
+        ["🌏 country", "Transylvania"],
+        ["🕟 time", "Transylvania"]
+    ],
+    "🇮🇳 India": [
+        ["🌏 country", "📕 Kama Sutra"]
+    ]
+}
